0a8b856fff2d2be89a54373500b76ba6ba8ef2f0
Mikko Suhonen <miksuh.dev@gmail.com>
2022-11-19 14:08:43 +0200

    fix: main menu mobile responsiveness

--- client/src/view/Main/index.tsx
+++ client/src/view/Main/index.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 const MainView: Component = () => {
   return (
     <div class="flex h-full items-center justify-center">
-      <h1 class="text-center text-9xl font-extrabold tracking-tight text-custom-primary-900 backdrop-blur backdrop-filter dark:text-white sm:text-7xl lg:text-8xl">
+      <h1 class="text-center text-7xl font-extrabold tracking-tight text-custom-primary-900 backdrop-blur backdrop-filter dark:text-white md:text-9xl">
         Hande
       </h1>
     </div>
