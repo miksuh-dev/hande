d7c655be74d62a071d996ab87526952e63e9c6e1
Mikko Suhonen <miksuh.dev@gmail.com>
2022-11-29 21:18:22 +0200

    fix: badge was in center if multiline message

--- client/src/components/Tooltip/index.tsx
+++ client/src/components/Tooltip/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Tooltip: Component<Props> = (props) => {
   return (
-    <div class="group relative flex cursor-pointer self-center whitespace-nowrap">
+    <div class="group relative mt-1 flex cursor-pointer whitespace-nowrap">
       {props.children}
       <div class="tooltip pointer-events-none absolute top-full -left-1/2 z-50 mt-2 p-2 text-center text-xs opacity-0 group-hover:opacity-100">
         {props.text}
