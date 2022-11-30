1d1a24e4dd963a17d839645d10fd20cf9871b2ec
Mikko Suhonen <miksuh.dev@gmail.com>
2022-11-30 17:17:16 +0200

    fix: tooltip added hidden space to the bottom of chat

--- client/src/components/Tooltip/index.tsx
+++ client/src/components/Tooltip/index.tsx
@@ -9,7 +9,7 @@ const Tooltip: Component<Props> = (props) => {
   return (
     <div class="group relative mt-1 flex cursor-pointer whitespace-nowrap">
       {props.children}
-      <div class="tooltip pointer-events-none absolute top-full -left-1/2 z-50 mt-2 p-2 text-center text-xs opacity-0 group-hover:opacity-100">
+      <div class="tooltip pointer-events-none absolute top-full -left-1/2 z-50 mt-2 hidden p-2 text-center text-xs opacity-0 group-hover:block group-hover:opacity-100">
         {props.text}
       </div>
     </div>
