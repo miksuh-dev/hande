b4ef2ab5e9ab812b9ccc08d5d42496f5274ee8f5
Mikko Suhonen <miksuh.dev@gmail.com>
2022-12-04 20:14:56 +0200

    feat: add icon to main menu

--- client/src/view/Main/index.tsx
+++ client/src/view/Main/index.tsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "@solidjs/router";
+import { CrownIcon } from "components/common/icon";
 import useAuth from "hooks/useAuth";
 import { Component, createEffect } from "solid-js";
 
@@ -21,6 +22,7 @@ const MainView: Component = () => {
   return (
     <div class="flex h-full items-center justify-center">
       <h1 class="text-center text-7xl font-extrabold tracking-tight text-custom-primary-900 backdrop-blur backdrop-filter dark:text-white md:text-9xl">
+        <CrownIcon />
         Hande
       </h1>
     </div>
