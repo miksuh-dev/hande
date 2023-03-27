eb5697ab6a9fe0b9223eb0c25ec175ce9e2d4da0
Mikko Suhonen <miksuh.dev@gmail.com>
2023-03-27 19:24:13 +0300

    fix: history footer buttons incorrect align
    History footer buttons stack incorrectly on mobile devices

--- client/src/view/Room/List/History/Footer.tsx
+++ client/src/view/Room/List/History/Footer.tsx
@@ -19,17 +19,17 @@ const FooterComponent: Component<Props> = (props) => {
 
   return (
     <div class="flex flex-1 items-center space-x-2">
-      <div class="flex-1 space-x-2">
+      <div class="-mt-2 flex-1 space-y-2">
         <button
           type="button"
-          class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
+          class="inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500 sm:mr-2"
           onClick={() => props.onSelectAll()}
         >
           {t("actions.selectAll")}
         </button>
         <button
           type="button"
-          class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
+          class="inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500 sm:mr-2"
           onClick={() => props.onClear()}
         >
           {t("actions.clearSelections")}
