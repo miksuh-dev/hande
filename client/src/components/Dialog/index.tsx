15f9244d45a70ac203fb0543d211d5ad34cc7bae
Mikko Suhonen <miksuh.dev@gmail.com>
2023-04-11 20:14:07 +0300

    refactor: reusable dialog component

---
+++ client/src/components/Dialog/index.tsx
@@ -0,0 +1,52 @@
+import { CrossIcon } from "components/common/icon";
+import { Component, JSX } from "solid-js";
+import { Portal } from "solid-js/web";
+import trackClickOutside from "utils/trackClickOutside";
+
+type Props = {
+  title: string;
+  children: JSX.Element;
+  actions?: JSX.Element;
+  onClose: () => void;
+};
+
+const PlaylistView: Component<Props> = (props) => {
+  return (
+    <Portal>
+      <div
+        class="tooltip fixed top-1/2 left-1/2 z-50 h-full w-full -translate-y-1/2 -translate-x-1/2 overflow-hidden outline-none md:h-3/4 md:w-3/4 2xl:w-1/2"
+        tabindex="-1"
+        role="dialog"
+        ref={(ref) => {
+          trackClickOutside(ref, () => {
+            props.onClose();
+          });
+        }}
+      >
+        <div class="oveflow-y-hidden pointer-events-none relative h-full w-auto">
+          <div class="pointer-events-auto relative flex h-full w-full flex-col rounded-md border-none bg-neutral-50 bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-900">
+            <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b border-neutral-200 p-4 dark:border-neutral-700">
+              <h5 class="text-xl font-medium leading-normal">{props.title}</h5>
+              <button
+                onClick={() => props.onClose()}
+                type="button"
+                class="icon-button h-8 w-8 p-1"
+                aria-label="Close"
+              >
+                <CrossIcon />
+              </button>
+            </div>
+            <div class="flex h-full flex-col space-y-4 overflow-hidden p-4">
+              <div class="relative h-full space-y-2 overflow-y-scroll">
+                {props.children}
+              </div>
+              {props.actions}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Portal>
+  );
+};
+
+export default PlaylistView;
--- client/src/components/PlaylistViewDialog/index.tsx
+++ client/src/components/PlaylistViewDialog/index.tsx
@@ -1,5 +1,5 @@
 import { useI18n } from "@solid-primitives/i18n";
-import { CircularLoadingSpinner, CrossIcon } from "components/common/icon";
+import { CircularLoadingSpinner } from "components/common/icon";
 import {
   Component,
   createResource,
@@ -7,12 +7,12 @@ import {
   For,
   Suspense,
 } from "solid-js";
-import { Portal } from "solid-js/web";
 import trpcClient from "trpc";
 import { htmlDecode } from "utils/parse";
 import { SearchResultPlaylist, SearchResultSong, Song } from "trpc/types";
 import Tooltip from "components/Tooltip";
 import SongThumbnail from "view/Room/common/SongThumbnail";
+import Dialog from "components/Dialog";
 
 type Props = {
   playlist: SearchResultPlaylist;
@@ -61,112 +61,90 @@ const PlaylistView: Component<Props> = (props) => {
   };
 
   return (
-    <Portal>
-      <div
-        class="tooltip fixed top-1/2 left-1/2 z-50 h-full w-full -translate-y-1/2 -translate-x-1/2 overflow-hidden outline-none md:h-3/4 md:w-3/4 2xl:w-1/2"
-        tabindex="-1"
-        role="dialog"
-      >
-        <div class="oveflow-y-hidden pointer-events-none relative h-full w-auto">
-          <div class="pointer-events-auto relative flex h-full w-full flex-col rounded-md border-none bg-neutral-50 bg-clip-padding text-current shadow-lg outline-none dark:bg-neutral-900">
-            <div class="flex flex-shrink-0 items-center justify-between rounded-t-md border-b border-neutral-200 p-4 dark:border-neutral-700">
-              <h5 class="text-xl font-medium leading-normal ">
-                {t("playlistDialog.title", {
-                  name: htmlDecode(props.playlist.title),
-                })}
-              </h5>
-              <button
-                onClick={() => props.onClose()}
-                type="button"
-                class="icon-button h-8 w-8 p-1"
-                aria-label="Close"
-              >
-                <CrossIcon />
-              </button>
-            </div>
-            <div class="flex h-full flex-col space-y-4 overflow-hidden p-4">
-              <div class="relative h-full space-y-2 overflow-y-scroll">
-                <Suspense
-                  fallback={
-                    <div class="flex h-full items-center justify-center">
-                      <span class="h-16 w-16">
-                        <CircularLoadingSpinner />
-                      </span>
-                    </div>
-                  }
-                >
-                  <For each={songs()}>
-                    {(song) => (
-                      <button
-                        class="card flex w-full cursor-default items-center justify-between p-2 px-4"
-                        classList={{
-                          "hover:bg-neutral-200 dark:hover:bg-neutral-600 cursor-pointer":
-                            !isInQueue(song),
-                        }}
-                        onClick={() => onSongSelect(song)}
-                      >
-                        <div class="flex items-center space-x-4">
-                          <input
-                            type="checkbox"
-                            class="h-4 w-4 rounded border-neutral-300 bg-neutral-100 text-custom-primary-600 dark:border-neutral-600 dark:bg-neutral-700"
-                            checked={selected().includes(song)}
-                            disabled={isInQueue(song)}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              onSongSelect(song);
-                            }}
-                          />
-                          <SongThumbnail song={song} />
-                          <div class="ml-4">
-                            <h3 class="text-md text-left font-medium text-neutral-900 dark:text-neutral-200">
-                              {htmlDecode(song.title)}
-                            </h3>
-                          </div>
-                        </div>
-                      </button>
-                    )}
-                  </For>
-                </Suspense>
-              </div>
-              <div class="flex">
-                <div class="flex-1 space-x-2">
-                  <button
-                    type="button"
-                    class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
-                    onClick={() => onSelectAll()}
-                  >
-                    {t("actions.selectAll")}
-                  </button>
-                  <button
-                    type="button"
-                    class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
-                    onClick={() => setSelected([])}
-                  >
-                    {t("actions.clearSelections")}
-                  </button>
-                </div>
-                <Tooltip
-                  text={t("tooltip.common.noSelections")}
-                  visible={selected().length === 0}
-                >
-                  <button
-                    type="button"
-                    class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
-                    onClick={() => {
-                      props.onSongAdd(selected());
-                      props.onClose();
-                    }}
-                    disabled={selected().length === 0}
-                  >
-                    {t("actions.addSelected")}
-                  </button>
-                </Tooltip>
-              </div>
-            </div>
+    <Dialog
+      title={t("playlistDialog.title", {
+        name: htmlDecode(props.playlist.title),
+      })}
+      onClose={() => props.onClose()}
+      actions={
+        <div class="flex">
+          <div class="flex-1 space-x-2">
+            <button
+              type="button"
+              class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
+              onClick={() => onSelectAll()}
+            >
+              {t("actions.selectAll")}
+            </button>
+            <button
+              type="button"
+              class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
+              onClick={() => setSelected([])}
+            >
+              {t("actions.clearSelections")}
+            </button>
           </div>
+          <Tooltip
+            text={t("tooltip.common.noSelections")}
+            visible={selected().length === 0}
+          >
+            <button
+              type="button"
+              class="ml-auto inline-flex shrink-0 items-center rounded border border-transparent bg-custom-primary-900 px-2.5 py-1.5 text-xs font-medium text-white hover:bg-custom-primary-800 focus:outline-none focus:ring-2 focus:ring-custom-primary-500 focus:ring-offset-2 dark:bg-custom-primary-900 dark:hover:bg-custom-primary-800 dark:focus:ring-custom-primary-500"
+              onClick={() => {
+                props.onSongAdd(selected());
+                props.onClose();
+              }}
+              disabled={selected().length === 0}
+            >
+              {t("actions.addSelected")}
+            </button>
+          </Tooltip>
         </div>
-      </div>
-    </Portal>
+      }
+    >
+      <Suspense
+        fallback={
+          <div class="flex h-full items-center justify-center">
+            <span class="h-16 w-16">
+              <CircularLoadingSpinner />
+            </span>
+          </div>
+        }
+      >
+        <For each={songs()}>
+          {(song) => (
+            <button
+              class="card flex w-full cursor-default items-center justify-between p-2 px-4"
+              classList={{
+                "hover:bg-neutral-200 dark:hover:bg-neutral-600 cursor-pointer":
+                  !isInQueue(song),
+              }}
+              onClick={() => onSongSelect(song)}
+            >
+              <div class="flex items-center space-x-4">
+                <input
+                  type="checkbox"
+                  class="h-4 w-4 rounded border-neutral-300 bg-neutral-100 text-custom-primary-600 dark:border-neutral-600 dark:bg-neutral-700"
+                  checked={selected().includes(song)}
+                  disabled={isInQueue(song)}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onSongSelect(song);
+                  }}
+                />
+                <SongThumbnail song={song} />
+                <div class="ml-4">
+                  <h3 class="text-md text-left font-medium text-neutral-900 dark:text-neutral-200">
+                    {htmlDecode(song.title)}
+                  </h3>
+                </div>
+              </div>
+            </button>
+          )}
+        </For>
+      </Suspense>
+    </Dialog>
   );
 };
 
